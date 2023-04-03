2000826f9902122777e467b11576196a5caf9d6f
Quinn Las <quinn.s.las@gmail.com>
2023-04-03 00:42:21 -0500

    CORS

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+  options.AddPolicy(
+    name: MyAllowSpecificOrigins,
+    policy => {
+      policy.WithOrigins("http://localhost:5173"); // TODO config
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapGet("/", () => "Hello World!");
 
