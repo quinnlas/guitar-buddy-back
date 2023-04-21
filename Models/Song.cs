f1a59de9a338023c166fd21996691ee1c4783efc
Quinn Las <quinn.s.las@gmail.com>
2023-04-20 19:39:15 -0500

    organize parsing algorithm and pass no spacer test

--- Models/Song.cs
+++ Models/Song.cs
@@ -6,20 +6,19 @@ public class Song
 
   public List<Measure> measures { get; }
 
-  public Song() {
+  public Song()
+  {
     this.measures = new List<Measure>();
   }
   public Song(string tab, int[] tuning)
   {
+    // convert tab -> tab blocks -> tab measures -> measures
     List<List<string>> blocks = getCleanedTabBlocks(tab);
-
-    this.measures = new List<Measure>();
-
-    foreach (List<string> block in blocks)
-    {
-      // a block can have multiple measures
-      this.measures.AddRange(parseBlockIntoMeasures(block, tuning));
-    }
+    List<List<string>> tabMeasures = blocks
+      .Select(block => convertBlockToTabMeasures(block))
+      .SelectMany(x => x) // since a block can have multiple measures, we need to flatten 1 level
+      .ToList();
+    this.measures = tabMeasures.Select(tm => parseTabMeasure(tm, tuning)).ToList();
   }
 
   public Song(TabForm tabForm) : this(tabForm.tab, tabForm.tuning) { }
@@ -39,18 +38,21 @@ public class Song
   // ~ is for vibrato
   private static Regex blockLineRegex = new Regex(@"\|[\-|\/\\\(\)_=~0-9hpbrso]*\|", RegexOptions.IgnoreCase);
   private static Regex noteRegex = new Regex(@"\d+");
+  private static Regex notSpacerRegex = new Regex(@"[^-]");
 
   /*
     returns a list of normalised blocks eg
 
-    |-------5-7-----7-|-8-----8-2-----2-|-0---------0-----|-----------------|
-    |-----5-----5-----|---5-------3-----|---1---1-----1---|-0-1-1-----------|
-    |---5---------5---|-----5-------2---|-----2---------2-|-0-2-2---2-------|
-    |-7-------6-------|-5-------4-------|-3---------------|-----------------|
-    |-----------------|-----------------|-----------------|-2-0-0---0--/8-7-|
-    |-----------------|-----------------|-----------------|-----------------|
+    e|-------5-7-----7-|-8-----8-2-----2-|-0---------0-----|-----------------|
+    B|-----5-----5-----|---5-------3-----|---1---1-----1---|-0-1-1-----------|
+    G|---5---------5---|-----5-------2---|-----2---------2-|-0-2-2---2-------|
+    D|-7-------6-------|-5-------4-------|-3---------------|-----------------|
+    A|-----------------|-----------------|-----------------|-2-0-0---0--/8-7-|
+    E|-----------------|-----------------|-----------------|-----------------|
 
+    removing lines from the tab text that are not actual tabulature
     each block is represented as a list of lines
+    there may or may not be characters before the first measure starts
   */
   private static List<List<string>> getCleanedTabBlocks(string tabText)
   {
@@ -79,53 +81,102 @@ public class Song
     return blocks;
   }
 
-  private static List<Measure> parseBlockIntoMeasures(List<string> block, int[] tuning)
-  {
-    List<Measure> measures = new List<Measure>();
+  /*
+    given a block such as:
+    e|-------5-7-----7-|-8-----8-2-----2-|-0---------0-----|-----------------|
+    B|-----5-----5-----|---5-------3-----|---1---1-----1---|-0-1-1-----------|
+    G|---5---------5---|-----5-------2---|-----2---------2-|-0-2-2---2-------|
+    D|-7-------6-------|-5-------4-------|-3---------------|-----------------|
+    A|-----------------|-----------------|-----------------|-2-0-0---0--/8-7-|
+    E|-----------------|-----------------|-----------------|-----------------|
 
-    // parse blocks one measure at a time
-    int measureStart = block[0].IndexOf('|') + 2; // TODO assumes one beginning spacer
-    int measureEnd = block[0].IndexOf('|', measureStart);
+    return a list where each element is one measure with the beginning space removed, such as:
+    ------5-7-----7-
+    ----5-----5-----
+    --5---------5---
+    7-------6-------
+    ----------------
+    ----------------
+    (this is one element of the list, each line is in a different string)
+  */
+  private static List<List<string>> convertBlockToTabMeasures(List<string> block)
+  {
+    List<string> remaining = block.Select(x => x).ToList();
+    List<List<string>> tabMeasures = new List<List<string>>();
+
     while (true)
     {
-      Measure measure = new Measure();
-      int measureLength = measureEnd - measureStart;
+      int leftBorder = remaining[0].IndexOf('|');
+      int rightBorder = remaining[0].IndexOf('|', leftBorder + 1);
 
-      // read notes from each line of the block
-      for (int lineIndex = 0; lineIndex < block.Count; lineIndex++)
+      if (rightBorder == -1) break;
+
+      // confirm lines match
+      for (int i = 1; i < remaining.Count; i++)
       {
-        string blockLine = block[lineIndex];
-        // trim to the current measure
-        string measureLine = blockLine.Substring(measureStart, measureLength);
-        MatchCollection noteMatches = noteRegex.Matches(measureLine);
-
-        foreach (Match noteMatch in noteMatches)
+        int rightBorderThisLine = remaining[i].IndexOf('|', leftBorder + 1);
+        if (remaining[i][leftBorder] != '|' || rightBorderThisLine != rightBorder)
         {
-          Note note = new Note();
-          note.pitch = tuning[lineIndex] + Int32.Parse(noteMatch.Value);
-          note.measureStart = ((float)noteMatch.Index) / (float)measureLength; // TODO simple rhythm guessing
-          measure.notes.Add(note);
+          throw new Exception("Found block where measure borders do not match between lines");
         }
       }
 
-      // sort measure by note order
-      measure.notes.Sort((Note a, Note b) =>
-      {
-        return a.measureStart.CompareTo(b.measureStart);
-      });
+      int numSpacers = remaining
+        .Select(l => notSpacerRegex.Match(l.Substring(leftBorder + 1)).Index)
+        .Min();
+      
+      int afterSpacers = leftBorder + numSpacers + 1;
+      int cleanMeasureLength = rightBorder - afterSpacers;
+      tabMeasures.Add(remaining.Select(l => l.Substring(afterSpacers, cleanMeasureLength)).ToList());
 
-      measures.Add(measure);
-
-      measureStart = measureEnd + 2;
-      if (measureStart >= block[0].Length) break;
-      measureEnd = block[0].IndexOf('|', measureStart);
-      if (measureEnd == -1) break;
+      // slurp
+      remaining = remaining.Select(l => l.Substring(rightBorder)).ToList();
     }
 
-    return measures;
+    return tabMeasures;
   }
 
-  public override bool Equals(object obj) {
+  /*
+    given a measure such as:
+    ------5-7-----7-
+    ----5-----5-----
+    --5---------5---
+    7-------6-------
+    ----------------
+    ----------------
+
+    parse it into a Measure object
+  */
+  private static Measure parseTabMeasure(List<string> tabMeasure, int[] tuning) {
+    Measure measure = new Measure();
+
+    // read notes from each line of the block
+    for (int lineIndex = 0; lineIndex < tabMeasure.Count; lineIndex++)
+    {
+      // trim to the current measure
+      string measureLine = tabMeasure[lineIndex];
+      MatchCollection noteMatches = noteRegex.Matches(measureLine);
+
+      foreach (Match noteMatch in noteMatches)
+      {
+        Note note = new Note();
+        note.pitch = tuning[lineIndex] + Int32.Parse(noteMatch.Value);
+        note.measureStart = ((float)noteMatch.Index) / (float) measureLine.Length; // TODO simple rhythm guessing
+        measure.notes.Add(note);
+      }
+    }
+
+    // sort measure by note order
+    measure.notes.Sort((Note a, Note b) =>
+    {
+      return a.measureStart.CompareTo(b.measureStart);
+    });
+
+    return measure;
+  }
+
+  public override bool Equals(object obj)
+  {
     if (obj.GetType() != typeof(Song)) return false;
 
     return this.measures.SequenceEqual(((Song)obj).measures);
